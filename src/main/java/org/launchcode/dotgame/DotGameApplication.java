15dcd6e84bc4a85b1704923c96e7c81d9c2cc91d
Jane Byrne <janebyrne92@gmail.com>
2020-04-19 19:34:25 -0500

    initial commit

---
+++ src/main/java/org/launchcode/dotgame/DotGameApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.dotgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/dotgame/DotGameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.dotgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
