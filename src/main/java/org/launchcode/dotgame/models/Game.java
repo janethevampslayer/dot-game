2ad01c84aa6e79cb223c560e1f180fab6408445a
Jane Byrne <janebyrne92@gmail.com>
2020-05-02 12:28:35 -0500

    Game Class created

--- src/main/java/org/launchcode/dotgame/models/Game.java
+++ src/main/java/org/launchcode/dotgame/models/Game.java
@@ -1,4 +1,20 @@
 package org.launchcode.dotgame.models;
 
+import java.util.Random;
+
 public class Game {
+
+    private int addendOne;
+    private int addendTwo;
+    private int sum;
+    private int[] gameSet = new int[3];
+
+    private void generateAddends() {
+        Random rand = new Random();
+        this.addendOne = rand.nextInt(1000);
+        this.addendTwo = rand.nextInt(1000);
+    }
+
+
+
 }
