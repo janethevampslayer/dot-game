abdab0b0316b77f2eedb82e6d287e73aac21b3d9
Jane Byrne <janebyrne92@gmail.com>
2020-05-15 16:43:15 -0500

    two ints are generated and passed to the view

--- src/main/java/org/launchcode/dotgame/controllers/GameController.java
+++ src/main/java/org/launchcode/dotgame/controllers/GameController.java
@@ -1,13 +1,19 @@
 package org.launchcode.dotgame.controllers;
 
+import org.launchcode.dotgame.models.Game;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GameController {
 
     @GetMapping("/newGame")
-    public String displayNewGame() {
+    public String displayNewGame(Model model) {
+        Game newGame = new Game();
+        newGame.generateAddends();
+        model.addAttribute("addendOne", newGame.getAddendOne());
+        model.addAttribute("addendTwo", newGame.getAddendTwo());
         return "/game/game-board";
     }
 }
--- src/main/java/org/launchcode/dotgame/models/Game.java
+++ src/main/java/org/launchcode/dotgame/models/Game.java
@@ -9,12 +9,29 @@ public class Game {
     private int sum = addendOne + addendTwo;
     private int[] gameSet = new int[3];
 
-    private void generateAddends() {
+    public int getAddendOne() {
+        return addendOne;
+    }
+
+    public int getAddendTwo() {
+        return addendTwo;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public int[] getGameSet() {
+        return gameSet;
+    }
+
+    public Game() {}
+
+    public void generateAddends() {
         Random rand = new Random();
         this.addendOne = rand.nextInt(1000);
         this.addendTwo = rand.nextInt(1000);
     }
-
 
 
 }
