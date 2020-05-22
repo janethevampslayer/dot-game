afade25554a68458810ead3a38535a757166dda9
Jane Byrne <janebyrne92@gmail.com>
2020-05-22 13:22:19 -0500

    current username added to view

--- src/main/java/org/launchcode/dotgame/controllers/UserController.java
+++ src/main/java/org/launchcode/dotgame/controllers/UserController.java
@@ -1,14 +1,19 @@
 package org.launchcode.dotgame.controllers;
 
+import org.launchcode.dotgame.AuthenticationFilter;
 import org.launchcode.dotgame.data.UserRepository;
 import org.launchcode.dotgame.models.User;
+import org.launchcode.dotgame.models.dto.LoginFormDTO;
+import org.launchcode.dotgame.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 //@RequestMapping("user")
@@ -18,8 +23,11 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping("home")
-    public String displayUserHome() {
-
+    public String displayUserHome(HttpSession session, Model model) {
+        Object userId = session.getAttribute("user");
+        Optional<User> result = userRepository.findById((Integer) userId);
+        User user = result.get();
+        model.addAttribute("username", user.getUsername());
         return "user/home";
     }
 
