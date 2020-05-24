ec13729272c5cf6ce5c20f469adb2cdd1769eeaa
Jane Byrne <janebyrne92@gmail.com>
2020-05-24 17:20:48 -0500

    dots in grid appear and disappear when clicked; alert message shows when correct answer is given

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,65 +1,75 @@
-var gridItem = document.getElementsByClassName("grid-item");
-var itemTen = document.getElementsByClassName("item-ten");
+var dotItem = document.getElementsByClassName("dot");
 
-for (var i = 0; i < gridItem.length; i++) {
-    gridItem[i].addEventListener("click", function() {
+for (var i = 0; i < dotItem.length; i++) {
+    dotItem[i].addEventListener("click", function() {
         if (this.style.backgroundColor != "black") {
             this.style.backgroundColor = "black";
-        } else if (this.style.backgroundColor = "black") {
-            this.style.backgroundColor = "";
-        }
-    })
-}
-
-for (var i = 0; i < itemTen.length; i++) {
-    itemTen[i].addEventListener("click", function() {
-        if (this.style.backgroundColor != "black") {
-            this.style.backgroundColor = "black";
-        } else if (this.style.backgroundColor = "black") {
-            this.style.backgroundColor = "";
+        } else if (this.style.backgroundColor = "black"){
+            this.style.backgroundColor = "white";
         }
     })
 }
 
 document.getElementById("reset-button").addEventListener("click", function() {
-    for (var i = 0; i < gridItem.length; i++) {
-        if (gridItem[i].style.backgroundColor = "black") {
-        gridItem[i].style.backgroundColor = "";
+    for (var i = 0; i < dotItem.length; i++) {
+        if (dotItem[i].style.backgroundColor = "black") {
+        dotItem[i].style.backgroundColor = "white";
         }
     }
-
-    for (var i = 0; i < itemTen.length; i++) {
-            if (itemTen[i].style.backgroundColor = "black") {
-            itemTen[i].style.backgroundColor = "";
-            }
-        }
+//    for (var i = 0; i < itemTen.length; i++) {
+//            if (itemTen[i].style.backgroundColor = "black") {
+//            itemTen[i].style.backgroundColor = "";
+//            }
+//        }
 })
-
 
 var checkAnswer = document.getElementById("check-answer-button");
 var addendOne = document.getElementById("addend-one").textContent;
 var addendTwo = document.getElementById("addend-two").textContent;
 var total = Number(addendOne) + Number(addendTwo);
 
-
-
 checkAnswer.addEventListener("click", function() {
-
-var input = Number(document.getElementById("answer").value);
-//              console.log(typeof(total));
-             console.log("type of total: " + typeof(total));
-             console.log(total);
-              console.log("type of input: " + typeof(input));
-              console.log(input);
+    var input = Number(document.getElementById("answer").value);
               console.log(total + input);
 
               if (total == input) {
-                console.log("CORRECT!");
+                confirm("correct!");
               } else {
                 console.log("silly button types");
               }
+})
 
 
-          })
+//var gridItem = document.getElementsByClassName("grid-item");
+//var itemTen = document.getElementsByClassName("item-ten");
+//
+//for (var i = 0; i < gridItem.length; i++) {
+//    gridItem[i].addEventListener("click", function() {
+//        if (this.style.backgroundColor != "black") {
+//            this.style.backgroundColor = "black";
+//        } else if (this.style.backgroundColor = "black") {
+//            this.style.backgroundColor = "";
+//        }
+//    })
+//}
+//
+//for (var i = 0; i < itemTen.length; i++) {
+//    itemTen[i].addEventListener("click", function() {
+//        if (this.style.backgroundColor != "black") {
+//            this.style.backgroundColor = "black";
+//        } else if (this.style.backgroundColor = "black") {
+//            this.style.backgroundColor = "";
+//        }
+//    })
+//}
+
+
+
+
+
+
+
+
+
 
 
