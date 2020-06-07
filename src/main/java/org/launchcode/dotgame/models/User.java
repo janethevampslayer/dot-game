606a47a9c872b50d95e531ac7785ac8e184ef8b2
Jane Byrne <janebyrne92@gmail.com>
2020-06-07 17:53:27 -0500

    error messages red and italic

--- src/main/java/org/launchcode/dotgame/models/User.java
+++ src/main/java/org/launchcode/dotgame/models/User.java
@@ -7,9 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
